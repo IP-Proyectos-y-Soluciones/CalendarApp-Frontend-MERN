7be31982ad2a5c20ce2f5924e9c40a91ad6b7ce3
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-06-26 15:43:34 -0500

    CalendarSlice

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -2,26 +2,17 @@ import { useState } from 'react';
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-import { addHours } from 'date-fns';
 import { CalendarEvent, CalendarModal, Navbar } from '../';
-import { getMessagesES, localizer } from '../../helpers';
-import { useUiStore } from '../../hooks';
 
-const events = [{
-  title: 'Reunión Preparatoria',
-  notes: 'Entregar información',
-  start: new Date(),
-  end: addHours( new Date(), 2 ),
-  bgColor: '#fafafa',
-  user: {
-    _id: '123',
-    name: 'Gerson',
-  },
-}];
+import { getMessagesES, localizer } from '../../helpers';
+import { useCalendarStore, useUiStore } from '../../hooks';
+
 
 export const CalendarPage = () => {
 
   const { openDateModal } = useUiStore();
+  const { events } = useCalendarStore();
+
   const [ lastView, setLastView ] = useState( localStorage.getItem( 'lastView' ) || 'week' );
 
   const eventStyleGetter = ( event, start, end, isSelected ) => {
---
+++ src/store/calendar/calendarSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { addHours } from 'date-fns';
+
+const tempEvent =   {
+  title: 'Reunión Preparatoria',
+  notes: 'Entregar información',
+  start: new Date(),
+  end: addHours( new Date(), 2 ),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Gerson',
+  },
+};
+
+export const calendarSlice = createSlice({
+  name: 'calendar',
+  initialState: {
+    events: [
+      tempEvent,
+    ],
+    activeEvent: null,
+  },
+  reducers: {
+    onSetActiveEvent: ( state, { payload } ) => {
+      state.activeEvent = payload;
+    },
+    onAddNewEvent: ( state, { payload } ) => {
+      state.events.push( payload );
+      state.activeEvent = null;
+    },
+  }
+});
+
+
+// Action creators are generated for each case reducer function
+export const { onAddNewEvent, onSetActiveEvent } = calendarSlice.actions;
