6a200acd2d663a556cd46dddbc1dcb8df5154e87
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-06-26 05:53:12 -0500

    Form validations

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,5 +1,8 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { addHours, differenceInSeconds } from 'date-fns';
+
+import Swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 import Modal from 'react-modal';
 
@@ -27,12 +30,23 @@ export const CalendarModal = () => {
 
   const [ isOpen, setIsOpen ] = useState( true );
 
+  const [ formSubmitted, setFormSubmitted ] = useState( false );
+
   const [ formValues, setFormValues ] = useState({
     title: 'Gerson',
     notes: 'Cualquier cosa',
     start: new Date(),
     end: addHours( new Date(), 2 ),
   });
+
+  const titleClass = useMemo( () => {
+    if ( !formSubmitted ) return '';
+
+    return ( formValues.title.length > 0 )
+      ? ''
+      : 'is-invalid';
+
+  }, [ formValues.title, formSubmitted ])
 
   const onInputChanged = ({ target }) => {
     setFormValues({
@@ -55,11 +69,13 @@ export const CalendarModal = () => {
 
   const onSubmit = ( event ) => {
     event.preventDefault();
+    setFormSubmitted( true );
 
     const difference = differenceInSeconds( formValues.end, formValues.start );
     
     if ( isNaN( difference ) || difference <= 0 ) {
       console.log('Error en fechas');
+      Swal.fire( 'Fechas incorrectas','Revisar las fechas ingresadas','error' );
       return;
     };
     
@@ -116,7 +132,7 @@ export const CalendarModal = () => {
           <label>Titulo y notas</label>
           <input
             type='text'
-            className='form-control'
+            className={ `form-control ${ titleClass }` }
             placeholder='Título del evento'
             name='title'
             autoComplete='off'
