f065e6d3b3f59b97a2714c5baa7f1165c5460b99
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-07-02 23:15:02 -0500

    userForm - Login and Register

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,7 +6,7 @@ import { getEnvVariables } from '../helpers';
 
 export const AppRouter = () => {
 
-  const authStatus = 'authenticated'; // 'authenticated'; // 'not-authenticated';
+  const authStatus = 'not-authenticated'; // 'authenticated'; // 'not-authenticated';
 
   console.log( getEnvVariables() );
 
