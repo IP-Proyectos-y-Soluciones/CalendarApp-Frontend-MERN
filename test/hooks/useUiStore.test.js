7e04a580a85c782ddd671c1b0b64823022662305
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-07-04 08:32:18 -0500

    Testing in the useUiStore

---
+++ test/hooks/useUiStore.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { configureStore } from '@reduxjs/toolkit';
+import { act, renderHook } from '@testing-library/react';
+import { useUiStore } from '../../src/hooks';
+import { Provider } from 'react-redux';
+import { uiSlice } from '../../src/store';
+
+
+const getMockStore = ( initialState ) => {
+  return configureStore({
+    reducer: {
+      ui: uiSlice.reducer
+    },
+    preloadedState: {
+      ui: { ...initialState }
+    }
+  });
+};
+
+
+describe('Pruebas en useUiStore', () => {
+  
+  test('debe de regresar los valores por defecto', () => {
+
+    const mockStore = getMockStore({ isDateModalOpen: false });
+    
+    const { result } = renderHook( () => useUiStore(), {
+      wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
+    });
+    expect( result.current ).toEqual({
+      isDateModalOpen: false,
+      closeDateModal: expect.any( Function ),
+      openDateModal: expect.any( Function ),
+      toggleDateModal: expect.any( Function ),
+    });
+  });
+});
