d6bf66a3f9edc19be02feef1ab719af18aa89f4f
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-07-04 10:29:53 -0500

    Testing functions connected to the store

--- test/hooks/useUiStore.test.js
+++ test/hooks/useUiStore.test.js
@@ -34,4 +34,54 @@ describe('Pruebas en useUiStore', () => {
       toggleDateModal: expect.any( Function ),
     });
   });
+
+  test('openDateModal debe de colocar true en el isDateModalOpen', () => {
+    
+    const mockStore = getMockStore({ isDateModalOpen: false });
+    const { result } = renderHook( () => useUiStore(), {
+      wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
+    });
+
+    const { openDateModal } = result.current;
+
+    act( () => {
+      openDateModal();
+    });
+
+    expect( result.current.isDateModalOpen ).toBeTruthy();
+  });
+
+  test('closeDateModal debe de colocar false en isDateModalOpen', () => {
+    
+    const mockStore = getMockStore({ isDateModalOpen: true });
+    const { result } = renderHook( () => useUiStore(), {
+      wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
+    });
+
+    const { closeDateModal } = result.current;
+
+    act( () => {
+      closeDateModal();
+    });
+
+    expect( result.current.isDateModalOpen ).toBeFalsy();
+  });
+
+  test('toggleDateModal debe de cambiar el estado respectivamente', () => {
+    
+    const mockStore = getMockStore({ isDateModalOpen: true });
+    const { result } = renderHook( () => useUiStore(), {
+      wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
+    });
+
+    act(() => {
+      result.current.toggleDateModal();
+    });
+    expect( result.current.isDateModalOpen ).toBeFalsy();
+    
+    act(() => {
+      result.current.toggleDateModal();
+    });
+    expect( result.current.isDateModalOpen ).toBeTruthy();
+  });
 });
