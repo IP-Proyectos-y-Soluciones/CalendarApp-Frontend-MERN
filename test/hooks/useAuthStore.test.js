0fdf2811cc686a1109890cbe85fe1f08ba9f7d66
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-07-05 14:46:45 -0500

    starLogin must fail authentication

--- test/hooks/useAuthStore.test.js
+++ test/hooks/useAuthStore.test.js
@@ -66,4 +66,30 @@ describe('Pruebas en useAuthStore', () => {
     expect( localStorage.getItem( 'token' ) ).toEqual( expect.any( String ) );
     expect( localStorage.getItem( 'token-init-date' ) ).toEqual( expect.any( String ) );
   });
+
+  test('startLogin debe de fallar la autenticación', async() => {
+    
+    localStorage.clear();
+    const mockStore = getMockStore({ ...notAuthenticatedState });
+
+    const { result } = renderHook( () => useAuthStore(), {
+      wrapper: ({ children }) => <Provider store={ mockStore } >{ children }</Provider>
+    });
+
+    await act( async() => {
+      await result.current.startLogin( { email: 'pedro@google.com', password: 'wrong-password' } )
+    });
+
+    const { errorMessage, status, user } = result.current;
+    expect( localStorage.getItem( 'token' ) ).toBe( null );
+    expect({ errorMessage, status, user }).toEqual({
+      errorMessage: 'Credenciales incorrectas',
+      status: 'not-authenticated',
+      user: {}
+    });
+
+    await waitFor( 
+      () => expect( result.current.errorMessage ).toBe( undefined )
+    );
+  });
 });
