de5c11559f498dde1eac3eeb2fe1996f6268a81a
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-07-05 15:47:22 -0500

    startRegister creation should fail

--- test/hooks/useAuthStore.test.js
+++ test/hooks/useAuthStore.test.js
@@ -124,4 +124,25 @@ describe('Pruebas en useAuthStore', () => {
     
     spy.mockRestore();
   });
+
+  test('startRegister debe de fallar la creación', async() => {
+    
+    const mockStore = getMockStore({ ...notAuthenticatedState });
+
+    const { result } = renderHook( () => useAuthStore(), {
+      wrapper: ({ children }) => <Provider store={ mockStore } >{ children }</Provider>
+    });
+
+    await act( async() => {
+      await result.current.startRegister( testUserCredentials )
+    });
+
+    const { errorMessage, status, user } = result.current;
+
+    expect({ errorMessage, status, user }).toEqual({
+      errorMessage: 'User already exists',
+      status: 'not-authenticated',
+      user: {},
+    });
+  });
 });
